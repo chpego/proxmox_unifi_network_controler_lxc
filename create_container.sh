4279f14143ec745d4aebd503de273ae4e6e805fd
chpego <chpego@chpego.fr>
2021-05-10 15:52:30 +0200

    correction URL

--- create_container.sh
+++ create_container.sh
@@ -65,7 +65,7 @@ TEMP_DIR=$(mktemp -d)
 pushd $TEMP_DIR >/dev/null
 
 # Download setup script
-wget -qL https://github.com/chpego/proxmox_unifi_network_controler_lxc/raw/master/setup.sh
+wget -qL https://github.com/chpego/proxmox_unifi_network_controler_lxc/raw/main/setup.sh
 
 # Detect modules and automatically load at boot
 load_module aufs
